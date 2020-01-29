b93f9cf73f8a9ee46ba3f931788f6c47ac041f93
David Castells-Rufas <david.castells@uab.cat>
2020-01-29 10:26:36 +0100

    4 and 5 dimension arrays

--- src/Matrix.h
+++ src/Matrix.h
@@ -35,4 +35,80 @@ public:
 	}
 };
 
+template<typename T>
+class Matrix4D
+{
+	T* m_data;
+	int m_d1;
+	int m_d2;
+        int m_d3;
+        int m_d4;
+        
+
+public:
+	Matrix4D(int d1, int d2, int d3, int d4)
+	{
+            m_d1 = d1;
+            m_d2 = d2;
+            m_d3 = d3;
+            m_d4 = d4;
+            
+		
+            m_data = new T[d1*d2*d3*d4];
+	}
+	
+	T get(int i1, int i2, int i3, int i4)
+	{
+            return m_data[(i1*m_d2*m_d2*m_d4)+(i2*m_d3*m_d4)+(i3*m_d4)+i4];
+	}
+	
+	void put(int i1, int i2, int i3, int i4, T v)
+	{
+            m_data[(i1*m_d2*m_d2*m_d4)+(i2*m_d3*m_d4)+(i3*m_d4)+i4] = v;
+	}
+        
+        T* get3DRef(int i1, int i2, int i3)
+        {
+            return &m_data[(i1*m_d2*m_d2*m_d4)+(i2*m_d3*m_d4)+(i3*m_d4)];
+        }
+};
+
+template<typename T>
+class Matrix5D
+{
+	T* m_data;
+	int m_d1;
+	int m_d2;
+        int m_d3;
+        int m_d4;
+        int m_d5;
+
+public:
+	Matrix5D(int d1, int d2, int d3, int d4, int d5)
+	{
+            m_d1 = d1;
+            m_d2 = d2;
+            m_d3 = d3;
+            m_d4 = d4;
+            m_d5 = d5;
+		
+            m_data = new T[d1*d2*d3*d4*d5];
+	}
+	
+	T get(int i1, int i2, int i3, int i4, int i5)
+	{
+            return m_data[(i1*m_d2*m_d2*m_d4*m_d5)+(i2*m_d3*m_d4*m_d5)+(i3*m_d4*m_d5)+(i4*m_d5)+i5];
+	}
+	
+	void put(int i1, int i2, int i3, int i4, int i5, T v)
+	{
+            m_data[(i1*m_d2*m_d2*m_d4*m_d5)+(i2*m_d3*m_d4*m_d5)+(i3*m_d4*m_d5)+(i4*m_d5)+i5] = v;
+	}
+        
+        T* get3DRef(int i1, int i2, int i3)
+        {
+            return &m_data[(i1*m_d2*m_d2*m_d4*m_d5)+(i2*m_d3*m_d4*m_d5)+(i3*m_d4*m_d5)];
+        }
+};
+
 #endif
